257a37b7558ae243992e265c6e56e91d481afc0a
Kyle Niezgoda <kniezgo@gmail.com>
2018-05-25 17:11:48 -0700

    fixes

--- pygoda.py
+++ pygoda.py
@@ -164,9 +164,7 @@ def eof(d, removeMeans = True, verbose = False):
 	D = np.zeros(shape = (M,M))
 	for ir in range(0,M):
 		if verbose:
-			print float(ir)/M
-			print round(float(ir)/M)
-			percent = round(float(ir)/M) * 100
+			percent = round(float(ir)/M * 100)
 			if percent % 10 == 0:
 				print str(percent) + "% done..."
 		D[ir,ir] = np.nanmean(d[:,ir] * d[:,ir])
