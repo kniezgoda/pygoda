e0c72d903cabe1428a08d808c75c0a377ef73c20
kniezgoda <kniezgo@gmail.com>
2023-03-20 15:07:05 -0500

    Update pygoda.py

--- pygoda.py
+++ pygoda.py
@@ -2,24 +2,6 @@
 # Other statistical and analysis tools have been added over time
 # kniezgo@gmail.com
 
-def plot_rigid(ax, x, y, rigid, alpha = 1):
-    '''
-    This function returns a plt subplot for drawing the rigid objects on a matplotlib axis
-    ax is the axis to plot the rigids on
-    x and y are the 1-dimensional x and y coordinates for the grid
-    Argument rigid should be the "rigid" variable from obstacles output
-    rigid needs to be the correct shape (i.e. 2d and correct axes for plotting)
-    '''
-    import numpy as np
-    import matplotlib.pyplot as plt
-    rigid_bool = np.zeros_like(rigid, dtype = 'bool')
-    rigid_bool[:] = rigid[:]
-    cmap_put = plt.cm.binary
-    rigid_mask = np.ma.masked_where(~rigid_bool.data, np.ones(rigid_bool.shape))
-    return ax.pcolormesh(xf,yf,rigid_mask,shading = 'auto', cmap=cmap_put, alpha = alpha, vmin=0, vmax=1)
-
-
-
 def runningMean(x, N, mode = 'same'):
     '''
     Computes the running mean of x over N steps
