8f4b5b4fb6d92567fcec11b2467f4fbb3bc7cbf9
kniezgoda <kniezgo@gmail.com>
2023-03-20 14:58:51 -0500

    Update pygoda.py

--- pygoda.py
+++ pygoda.py
@@ -10,6 +10,8 @@ def plot_rigid(ax, x, y, rigid, alpha = 1, cmap = plt.cm.binary):
     Argument rigid should be the "rigid" variable from obstacles output
     rigid needs to be the correct shape (i.e. 2d and correct axes for plotting)
     '''
+    import numpy as np
+    import matplotlib.pyplot as plt
     rigid_bool = np.zeros_like(rigid, dtype = 'bool')
     rigid_bool[:] = rigid[:]
     rigid_mask = np.ma.masked_where(~rigid_bool.data, np.ones(rigid_bool.shape))
